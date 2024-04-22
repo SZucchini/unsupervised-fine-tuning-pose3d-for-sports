a1713c1b3681f87a7758f2c58febadc3586ab1f5
SZucchini <suzuki.tomohiro@g.sp.m.is.nagoya-u.ac.jp>
2024-04-22 21:05:17 +0900

    del H36M option

--- asp/dataset.py
+++ asp/dataset.py
@@ -26,30 +26,16 @@ def flip_data(data, left=[1, 2, 3, 14, 15, 16], right=[4, 5, 6, 11, 12, 13]):
 
 
 class MotionDataset3D(Dataset):
-    def __init__(self, cfg, data_root, test=False, flip=True):
+    def __init__(self, data_root, test=False, flip=True):
         self.data_root = data_root
         self.data_split = data_root.split('/')[-1]
-        if ('train' in self.data_split) and cfg.TRAIN.WITH_H36M:
-            self.data_root = [self.data_root, cfg.DATA.H36M_PATH]
-            self.h36m_ratio = 1
         self.flip = flip
         self.test = test
         self.file_list = self._generate_file_list()
 
     def _generate_file_list(self):
-        if isinstance(self.data_root, list):
-            file_list = []
-            for i, data_root in enumerate(self.data_root):
-                files = natsorted(glob.glob(data_root + '/*.pkl'))
-                if i == 0:
-                    file_num = len(files)
-                if i == 1:
-                    file_num = int(file_num * self.h36m_ratio)
-                    files = random.sample(files, file_num)
-                file_list += files
-        else:
-            files = glob.glob(self.data_root + '/*.pkl')
-            file_list = natsorted(files)
+        files = glob.glob(self.data_root + '/*.pkl')
+        file_list = natsorted(files)
         return file_list
 
     def __len__(self):
